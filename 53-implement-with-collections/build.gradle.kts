b5e038ca1f21be9462abea3d76b25075905cbd12
GiovanniP <giovanni.perreon@studio.unibo.it>
2025-05-10 11:40:02 +0200

    AddedLab5Files

---
+++ 53-implement-with-collections/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    application
+    java
+    id("org.danilopianini.gradle-java-qa") version "1.71.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("it.unibo.collections.design.UseWarehouse")
+}
+
+val runOrderedWarehouse by tasks.registering(JavaExec::class) {
+    mainClass.set("it.unibo.collections.design.UseOrderedWarehouse")
+    classpath(sourceSets.main.get().runtimeClasspath)
+}
+
+tasks.run.get().dependsOn(runOrderedWarehouse)
